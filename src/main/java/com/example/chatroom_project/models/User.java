76fd36eaa1519aac5d88c29e0e424f6bf2393a20
Nasthasia Usoh <nasthasia@hotmail.co.uk>
2023-04-24 17:15:05 +0100

    completed user model

--- src/main/java/com/example/chatroom_project/models/User.java
+++ src/main/java/com/example/chatroom_project/models/User.java
@@ -1,4 +1,74 @@
 package com.example.chatroom_project.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnoreProperties({"user"})
+    private List<Message> messages;
+
+    public User(String name, String email){
+        this.name = name;
+        this.email = email;
+        this.messages = new ArrayList<>();
+    }
+
+
+//    Default constructor
+    public User(){
+
+    }
+
+//    Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+
 }

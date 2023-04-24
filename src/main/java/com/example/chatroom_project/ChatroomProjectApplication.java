80c813ba4ec26e915a47b3152821df27c4d8d345
Sujan Sriram <sujansriram9@gmail.com>
2023-04-24 11:14:22 +0100

    first commit

---
+++ src/main/java/com/example/chatroom_project/ChatroomProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatroom_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatroomProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatroomProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatroom_project/ChatroomProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatroom_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatroomProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

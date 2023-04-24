e9469a5b1a36db31d1ef308f9a92c435624e8db8
Sarah Abdulle <sarahabdulle1@yahoo.com>
2023-04-24 17:47:32 +0100

    removed git class from message model

--- src/main/java/com/example/chatroom_project/models/Message.java
+++ src/main/java/com/example/chatroom_project/models/Message.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 @Table(name = "messages")
 public class Message {
 
-//    PROPERTIES
+    //    PROPERTIES
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column

ed98c736892df4869d4622da0170864012b64222
Nasthasia Usoh <nasthasia@hotmail.co.uk>
2023-04-25 10:28:58 +0100

    adding user controller service and repo

--- src/main/java/com/example/chatroom_project/controllers/UserController.java
+++ src/main/java/com/example/chatroom_project/controllers/UserController.java
@@ -1,4 +1,49 @@
 package com.example.chatroom_project.controllers;
 
+import com.example.chatroom_project.models.User;
+import com.example.chatroom_project.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+
+    @GetMapping
+    public ResponseEntity<List<User>> displayAllUsers(){
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> displayUserById (@PathVariable Long id) {
+       return new ResponseEntity<>(userService.getUserById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        return new ResponseEntity<>(userService.createUser(user), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+        return new ResponseEntity<>(String.format("user of id %s has been deleted", id), HttpStatus.OK);
+    }
+
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<User> changeEmail(@PathVariable Long id, @RequestBody String email){
+        return new ResponseEntity<>(userService.updateUserEmail(email, id), HttpStatus.OK);
+    }
+
+
+
 }
--- src/main/java/com/example/chatroom_project/services/UserService.java
+++ src/main/java/com/example/chatroom_project/services/UserService.java
@@ -1,4 +1,40 @@
 package com.example.chatroom_project.services;
 
+import com.example.chatroom_project.models.User;
+import com.example.chatroom_project.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).get();
+    }
+
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public User updateUserEmail(String email, Long userId){
+        User user = userRepository.findById(userId).get();
+        user.setEmail(email);
+        return userRepository.save(user);
+    }
 }
